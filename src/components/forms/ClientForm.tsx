37cdad50fb82d7c399e5abbe891a12392325f9c1
HadamaSAMASSA <samassaadama@hotmail.fr>
2025-09-23 17:21:56 +0300

    design frais km ClientForm

--- src/components/forms/ClientForm.tsx
+++ src/components/forms/ClientForm.tsx
@@ -215,11 +215,11 @@ export const ClientForm: React.FC<ClientFormProps> = ({ data, errors, onChange, 
                                 className="mt-2 p-3 bg-orange-50 border border-orange-200 rounded-md"
                             >
                                 <div className="flex items-start">
-                                    <div className="flex-shrink-0">
+                                    {/* <div className="flex-shrink-0">
                                         <svg className="h-5 w-5 text-orange-400" viewBox="0 0 20 20" fill="currentColor">
                                             <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                                         </svg>
-                                    </div>
+                                    </div> */}
                                     <div className="ml-3">
                                         <h3 className="text-sm font-medium text-orange-800">
                                             Adresse hors zone forfait
@@ -299,11 +299,11 @@ export const ClientForm: React.FC<ClientFormProps> = ({ data, errors, onChange, 
                                 className="mt-2 p-3 bg-orange-50 border border-orange-200 rounded-md"
                             >
                                 <div className="flex items-start">
-                                    <div className="flex-shrink-0">
+                                    {/* <div className="flex-shrink-0">
                                         <svg className="h-5 w-5 text-orange-400" viewBox="0 0 20 20" fill="currentColor">
                                             <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                                         </svg>
-                                    </div>
+                                    </div> */}
                                     <div className="ml-3">
                                         <h3 className="text-sm font-medium text-orange-800">
                                             Adresse hors zone forfait
