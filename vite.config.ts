59d11266db8baa4182fb091ed6566b286333771b
HadamaSAMASSA <samassaadama@hotmail.fr>
2025-12-20 08:02:51 +0200

    Mise en place electron OK + suppression complète des liens avec Airtable OK 2e tentative de push

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB (au lieu de 2 MB par défaut)
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
         runtimeCaching: [
           {
